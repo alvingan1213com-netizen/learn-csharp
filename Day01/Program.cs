3967964617e60d4d31f7db0f832595e6229d62f2
Shi Wei <gan.shiwei1213@gmail.com>
2026-08-20 19:24:36 +0800

    feat: add simple calculator function

--- Day01/Program.cs
+++ Day01/Program.cs
@@ -1,3 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("我是 B 版");
+
+int Add(int a, int b) => a + b;
+Console.WriteLine($"3 + 5 = {Add(3, 5)}");
