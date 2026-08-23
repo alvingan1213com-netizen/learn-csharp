452e4481d3dd9a42347badac969eb90975676398
Shi Wei <gan.shiwei1213@gmail.com>
2026-08-23 19:04:44 +0800

    feat: add subtract function

--- Day01/Program.cs
+++ Day01/Program.cs
@@ -4,3 +4,5 @@ Console.WriteLine("我是 B 版");
 
 int Add(int a, int b) => a + b;
 Console.WriteLine($"3 + 5 = {Add(3, 5)}");
+int Subtract(int a, int b) => a - b;
+Console.WriteLine($"9 - 4 = {Subtract(9, 4)}");
