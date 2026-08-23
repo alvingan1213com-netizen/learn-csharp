f3ce06a9b5d235b7fa2fc250b6ae4f1db24bfca5
Shi Wei <gan.shiwei1213@gmail.com>
2026-08-23 19:05:33 +0800

    feat: add multiply function

--- Day01/Program.cs
+++ Day01/Program.cs
@@ -4,3 +4,5 @@ Console.WriteLine("我是 B 版");
 
 int Add(int a, int b) => a + b;
 Console.WriteLine($"3 + 5 = {Add(3, 5)}");
+int Multiply(int a, int b) => a * b;
+Console.WriteLine($"6 * 3 = {Multiply(6, 3)}");
