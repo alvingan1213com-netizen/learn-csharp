25f651def990a8130c555e430b18e384a9cf5a05
Shi Wei <gan.shiwei1213@gmail.com>
2026-08-30 11:12:33 +0800

    feat: print command line arguments

--- Day01/Program.cs
+++ Day01/Program.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+class Order { }              // ❌ 先定义了 class
+Console.WriteLine("hi");     // 顶层陈述式在后面
 Console.WriteLine("Hello, World!");
 Console.WriteLine("我是 B 版");
 
@@ -14,3 +16,5 @@ int Divide(int a, int b)
     return a / b;
 }
 Console.WriteLine($"10 / 2 = {Divide(10, 2)}");
+Console.WriteLine($"收到 {args.Length} 个参数");
+foreach (var a in args) Console.WriteLine($"  - {a}");
